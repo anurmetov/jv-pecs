3ac2f1374e30ef2285b9a6d88325434b17a1272d
artem <artemnurmetow@gmail.com>
2025-09-24 21:26:46 +0200

    - improved code due to review

--- src/main/java/core/mate/academy/service/MachineServiceImpl.java
+++ src/main/java/core/mate/academy/service/MachineServiceImpl.java
@@ -4,6 +4,7 @@ import core.mate.academy.model.Bulldozer;
 import core.mate.academy.model.Excavator;
 import core.mate.academy.model.Machine;
 import core.mate.academy.model.Truck;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,17 +15,21 @@ public class MachineServiceImpl implements MachineService<Machine> {
     @Override
     public List<Machine> getAll(Class<? extends Machine> type) {
         if (type == Bulldozer.class) {
-            BulldozerProducer p = new BulldozerProducer();
-            return p.get();
-        } else if (type == Excavator.class) {
-            ExcavatorProducer p = new ExcavatorProducer();
-            return p.get();
-        } else if (type == Truck.class) {
-            TruckProducer p = new TruckProducer();
-            return p.get();
-        } else {
-            return Collections.emptyList();
+            BulldozerProducer producer = new BulldozerProducer();
+            List<? extends Machine> machines = producer.get();
+            return new ArrayList<>(machines);
         }
+        if (type == Excavator.class) {
+            ExcavatorProducer producer = new ExcavatorProducer();
+            List<? extends Machine> machines = producer.get();
+            return new ArrayList<>(machines);
+        }
+        if (type == Truck.class) {
+            TruckProducer producer = new TruckProducer();
+            List<? extends Machine> machines = producer.get();
+            return new ArrayList<>(machines);
+        }
+        return Collections.emptyList();
     }
 
     @Override
@@ -37,9 +42,7 @@ public class MachineServiceImpl implements MachineService<Machine> {
     @Override
     public void startWorking(List<? extends Machine> machines) {
         for (Machine machine : machines) {
-            if (machine.getClass() == Bulldozer.class || machine.getClass() == Truck.class) {
-                machine.doWork();
-            }
+            machine.doWork();
         }
     }
 
